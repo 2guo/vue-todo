737a8e1979f8d6cbe64c6f5d98c60fb425410220
Anson Guo <gja1026@163.com>
2021-01-24 01:03:00 +0800

    feat(project): init project with webpack4.x

---
+++ build/webpack.config.client.js
@@ -0,0 +1,96 @@
+const path = require("path");
+const HTMLPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const ExtractPlugin = require('extract-text-webpack-plugin')
+const baseConfig = require("./webpack.config.base");
+
+const isDev = process.env.NODE_ENV === "development";
+
+const defaultPluins = [
+  new webpack.DefinePlugin({
+    "process.env": {
+      NODE_ENV: isDev ? '"development"' : '"production"',
+    },
+  }),
+  new HTMLPlugin(),
+];
+
+const devServer = {
+  port: 8000,
+  host: "0.0.0.0",
+  overlay: {
+    errors: true,
+  },
+  hot: true,
+};
+
+let config;
+
+if (isDev) {
+  config = merge(baseConfig, {
+    devtool: "#cheap-module-eval-source-map",
+    module: {
+      rules: [
+        {
+          test: /\.styl/,
+          use: [
+            "vue-style-loader",
+            "css-loader",
+            {
+              loader: "postcss-loader",
+              options: {
+                sourceMap: true,
+              },
+            },
+            "stylus-loader",
+          ],
+        },
+      ],
+    },
+    devServer,
+    plugins: defaultPluins.concat([
+      new webpack.HotModuleReplacementPlugin(),
+    ]),
+  });
+} else {
+  config = merge(baseConfig, {
+    entry: {
+      app: path.join(__dirname, "../client/index.js"),
+    },
+    output: {
+      filename: "[name].[chunkhash:8].js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.styl/,
+          use: ExtractPlugin.extract({
+            fallback: 'vue-style-loader',
+            use: [
+              'css-loader',
+              {
+                loader: 'postcss-loader',
+                options: {
+                  sourceMap: true,
+                }
+              },
+              'stylus-loader'
+            ]
+          })
+        },
+      ],
+    },
+    optimization: {
+      splitChunks: {
+        chunks: "all",
+      },
+      runtimeChunk: true,
+    },
+    plugins: defaultPluins.concat([
+      new ExtractPlugin('styles.[md5:contentHash:hex:8].css'), // https://blog.csdn.net/shenzhen_zsw/article/details/97943217
+    ]),
+  });
+}
+
+module.exports = config;
